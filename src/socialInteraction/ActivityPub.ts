e9d9e16e1ffdc85ab6d08c6935304830bf546e79
Mitch Downey <mitchldowney@gmail.com>
2022-03-19 17:32:40 -0500

    Add removeUsernamesFromBeginningOfString helper

--- src/socialInteraction/ActivityPub.ts
+++ src/socialInteraction/ActivityPub.ts
@@ -16,3 +16,17 @@ export const checkIfAllowedImageOrigin = (commenter: any) => {
     return commenter.url && commenter.url.indexOf(origin) >= 0
   })
 }
+
+export const removeUsernamesFromBeginningOfString = (str: string) => {
+  const regex = /^\B\@([\w\-]+) /
+  let newStr = str
+  let replaced = true
+  do {
+    replaced = newStr.search(regex) >= 0
+    if (replaced) {
+      newStr = newStr.replace(regex, '')
+    }
+  } while (replaced)
+  
+  return newStr
+}
