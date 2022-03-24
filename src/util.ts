aba383b96fc159f8b7902fb95b85097dda659c32
Mitch Downey <mitchldowney@gmail.com>
2022-03-24 01:03:26 -0500

    Add getLocationURL

--- src/util.ts
+++ src/util.ts
@@ -3,3 +3,17 @@ import { decode } from 'html-entities'
 export const decodeHtml = (html = '') => {
   return decode(html)
 }
+
+export const getLocationURL = (href: string) => {
+  var match = href.match(/^(https?\:)\/\/(([^:\/?#]*)(?:\:([0-9]+))?)([\/]{0,1}[^?#]*)(\?[^#]*|)(#.*|)$/);
+  return match && {
+    href: href,
+    protocol: match[1],
+    host: match[2],
+    hostname: match[3],
+    port: match[4],
+    pathname: match[5],
+    search: match[6],
+    hash: match[7]
+  }
+}
