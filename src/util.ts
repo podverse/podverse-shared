3d5bb2cbf1d8eaaa995a5da0d1644b27a01c29bb
Mitch Downey <mitchldowney@gmail.com>
2022-03-24 00:14:05 -0500

    Use html-entities decode instead of document in decodeHtml helper

--- src/util.ts
+++ src/util.ts
@@ -1,5 +1,5 @@
+import { decode } from 'html-entities'
+
 export const decodeHtml = (html = '') => {
-  const txt = document.createElement('textarea')
-  txt.innerHTML = html
-  return txt.value
+  return decode(html)
 }
