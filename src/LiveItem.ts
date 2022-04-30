33e8aa78309d24a176ff4e87701f0f52b1b9512c
Mitch Downey <mitchldowney@gmail.com>
2022-04-30 11:52:49 -0500

    Update LiveItem.ts

--- src/LiveItem.ts
+++ src/LiveItem.ts
@@ -1,4 +1,4 @@
-import { Episode } from ".";
+import { Episode, ParsedEpisode } from ".";
 
 export type LiveItemStatus = 'pending' | 'live' | 'ended' | 'none'
 
@@ -7,4 +7,26 @@ export type LiveItem = {
   start: Date
   end: Date | null
   episode: Episode
+}
+
+export const parseLatestLiveItemStatus = (parsedLiveItemEpisodes: ParsedEpisode[]) => {
+  let latestLiveItemStatus = 'none' as LiveItemStatus
+  for (const parsedLiveItemEpisode of parsedLiveItemEpisodes) {
+    if (parsedLiveItemEpisode.liveItemStatus === 'live') {
+      latestLiveItemStatus = 'live'
+      break
+    } else if (
+      parsedLiveItemEpisode.liveItemStatus === 'pending'
+      && latestLiveItemStatus !== 'live'
+    ) {
+      latestLiveItemStatus = 'pending'
+    } else if (
+      parsedLiveItemEpisode.liveItemStatus === 'ended'
+      && latestLiveItemStatus !== 'live'
+      && latestLiveItemStatus !== 'pending'
+    ) {
+      latestLiveItemStatus = 'ended'
+    }
+  }
+  return latestLiveItemStatus
 }
