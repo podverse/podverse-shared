1503de1322116c63e50811b828a1237ea100fbcf
Mitch Downey <mitchldowney@gmail.com>
2023-03-06 20:57:28 -0600

    Update Translators.ts

--- src/Translators.ts
+++ src/Translators.ts
@@ -15,12 +15,12 @@ export const parseTranslatorsSection = (language: string, str: string) => {
     translators: [],
   };
   for (const split of splits) {
-    const name = split;
+    const name = split || '';
     // eslint-disable-next-line @typescript-eslint/prefer-regexp-exec
     const regexResults = name.match(new RegExp("<(.*?)>"));
     const url = regexResults && regexResults[1];
     const translator: Translator = {
-      name: url && regexResults && name ? name.substring(0, regexResults.index) : name,
+      name: url && regexResults && name ? name.substring(0, regexResults.index).trim() : name.trim(),
       url,
     };
 
