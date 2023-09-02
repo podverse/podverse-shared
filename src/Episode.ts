05e8ed85c736824f648c8e52e27bc1285d021f14
Mitch Downey <mitchldowney@gmail.com>
2023-09-02 14:54:14 -0500

    Add itunes season columns to Episode type

--- src/Episode.ts
+++ src/Episode.ts
@@ -14,6 +14,9 @@ export type Episode = {
   imageUrl?: string | null
   isExplicit: boolean
   isPublic: boolean
+  itunesEpisode?: any;
+  itunesEpisodeType?: string;
+  itunesSeason?: any;
   linkUrl?: string | null
   liveItem: LiveItem
   mediaFilesize: number
