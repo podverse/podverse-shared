9f11af67ef81c78aea53002a5312280eb51b5814
Mitch Downey <mitchldowney@gmail.com>
2023-12-10 22:16:46 -0600

    Use wildcard for imports

--- src/index.ts
+++ src/index.ts
@@ -1,49 +1,34 @@
-export { extractSelectedEnclosureSourceAndContentType } from './alternateEnclosures'
-export { Author, generateAuthorsText } from './Author'
-export { Category, generateCategoriesText, generateCategoryItems } from './Category'
-export { getUsernameAndPasswordFromCredentials } from './credentials'
-export { Episode, EpisodeAlternateEnclosure, EpisodeAlternateEnclosureSource, EpisodeContentLinks,
-  ParsedEpisode } from './Episode'
-export { FeedUrl, getAuthorityFeedUrlFromArray } from './FeedUrl'
-export { Funding } from './funding'
-export { decodeHTMLString, filterHTMLElementsFromString, removeLinebreaks,
-  replaceLinebreaksWithBrTags, removeExtraInfoFromEpisodeDescription, removeHTMLFromString } from './htmlHelpers'
-export { LiveItem, LiveItemStatus, liveItemStatuses, parseLatestLiveItemStatus, parseLatestLiveItemInfo } from './LiveItem'
-export { MediaRef, getMediaRefStartPosition } from './MediaRef'
-export { PodcastMedium, podcastMediumAllowedValues } from './medium'
-export {
-  NowPlayingItem,
-  checkIfNowPlayingItem,
-  checkIfSameNowPlayingItems,
-  checkIfVideoFileType,
-  checkIfVideoFileOrVideoLiveType,
-  cleanNowPlayingItem,
-  convertNowPlayingItemToEpisode,
-  convertNowPlayingItemToMediaRef,
-  convertNowPlayingItemClipToNowPlayingItemEpisode,
-  convertToNowPlayingItem,
-  haveNowPlayingItemsChanged
-} from './nowPlayingItem'
-export { parseOpmlFile } from './opml'
-export { Playlist, combineAndSortPlaylistItems } from './Playlist'
-export { Podcast, podcastItunesTypeDefaultValue } from './Podcast'
-export { SatoshiStreamStats, SatoshiStreamStatsPodcast } from './satoshiStream'
-export { getSeasonOrSerialEpisodesData } from './seasons'
-export { checkIfAllowedImageOrigin, removeUsernamesFromBeginningOfString } from './socialInteraction/ActivityPub'
-export { PVComment } from './socialInteraction/PVComment'
-export { ThreadcapAttachment, ThreadcapCommenter, ThreadcapCommenterIcon,
-  ThreadcapNode, ThreadcapNodeComment, ThreadcapResponse, convertThreadcapResponseToPVComment } from './socialInteraction/Threadcap'
-export { SocialInteraction, SocialInteractionKeys, checkIfHasSupportedCommentTag } from './socialInteraction/SocialInteraction'
-export { convertHHMMSSToAnchorTags, convertHHMMSSToSeconds, convertHoursMinutesSecondsToSeconds, convertSecToFullHHMMSS, convertSecToHHMMSS, convertSecToHhoursMMinutes, getHHMMSSArray, getHHMMSSMatchesInString, getTimeLabelText, validateHHMMSSString } from './timeHelpers'
-export { parseTranslatorsSection, Translator, TranslatorsSection } from './Translators'
-export { Transcript, TranscriptRow, TranscriptType, getTranscriptForLocale, parseTranscriptFile } from './transcript'
-export { addParameterToURL, convertUrlToSecureHTTPS, createEmailLinkUrl, generateQueryParams, getExtensionFromUrl, getLocationURL } from './urls'
-export { addLightningBoltToString, checkIfIsLightningKeysendValueTag, getLightningKeysendValueItem,
-  ValueRecipient, ValueRecipientNormalized, ValueTagExtended, ValueTagOriginal,
-  ValueTimeSplit, ValueTransaction } from './valueTag'
-export { User } from './User'
-export { UserHistoryItem } from './UserHistoryItem'
-export { UserNowPlayingItem } from './UserNowPlayingItem'
-export { UserQueueItem } from './UserQueueItem'
-export { checkIfIdMatchesClipIdOrEpisodeIdOrAddByUrl, convertBytesToHumanReadableString, encodeSpacesInString, numberWithCommas, overrideImageUrlWithChapterImageUrl, parseCommaDelimitedNamesAndURLsString, removeArticles, capitalizeFirstLetter, convertToSlug } from './util'
-export { checkIfContainsStringMatch, hasAtLeastXCharacters, hasLowercase, hasMatchingStrings, hasNumber, hasUppercase, isOdd, isValidDate, isValidUrl } from './validators'
+export * from './alternateEnclosures'
+export * from './Author'
+export * from './Category'
+export * from './credentials'
+export * from './Episode'
+export * from './FeedUrl'
+export * from './funding'
+export * from './htmlHelpers'
+export * from './LiveItem'
+export * from './logger'
+export * from './MediaRef'
+export * from './medium'
+export * from './nowPlayingItem'
+export * from './opml'
+export * from './Playlist'
+export * from './Podcast'
+export * from './satoshiStream'
+export * from './seasons'
+export * from './services/podcastIndex'
+export * from './socialInteraction/ActivityPub'
+export * from './socialInteraction/PVComment'
+export * from './socialInteraction/Threadcap'
+export * from './socialInteraction/SocialInteraction'
+export * from './timeHelpers'
+export * from './Translators'
+export * from './transcript'
+export * from './urls'
+export * from './valueTag'
+export * from './User'
+export * from './UserHistoryItem'
+export * from './UserNowPlayingItem'
+export * from './UserQueueItem'
+export * from './util'
+export * from './validators'
