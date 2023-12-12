0b00be52a5a234a18b538b0256a1066f6f2b3403
Mitch Downey <mitchldowney@gmail.com>
2023-12-12 03:28:42 -0600

    Update LiveItem.ts

--- src/LiveItem.ts
+++ src/LiveItem.ts
@@ -1,65 +1,23 @@
-import { Episode, ParsedEpisode } from "."
+import { Episode } from "."
 
 export type LiveItemStatus = 'pending' | 'live' | 'ended' | 'none'
 
 export const liveItemStatuses = ['live', 'pending', 'ended']
 
 export type LiveItem = {
-  status: LiveItemStatus
-  start: Date
+  // ids
+  id: string
+
+  // other props
+  chatIRCURL?: string | null
   end: Date | null
+  start: Date
+  status: LiveItemStatus
+  
+  // relationships
   episode: Episode
-  chatIRCURL: string | null
-}
 
-export type ParsedLiveItem = {
-  chat?: string
-  episode: ParsedEpisode
-  end?: Date | null
-  start?: Date
-  status?: LiveItemStatus
-}
-
-export const parseLatestLiveItemStatus = (parsedLiveItems: ParsedLiveItem[]) => {
-  let latestLiveItemStatus = 'none' as LiveItemStatus
-  for (const parsedLiveItem of parsedLiveItems) {
-    const liveItemStatus = parsedLiveItem.status?.toLowerCase()
-    if (liveItemStatus === 'live') {
-      latestLiveItemStatus = 'live'
-      break
-    } else if (
-      liveItemStatus === 'pending'
-      && latestLiveItemStatus !== 'live'
-    ) {
-      latestLiveItemStatus = 'pending'
-    } else if (
-      liveItemStatus === 'ended'
-      && latestLiveItemStatus !== 'live'
-      && latestLiveItemStatus !== 'pending'
-    ) {
-      latestLiveItemStatus = 'ended'
-    }
-  }
-  return latestLiveItemStatus
-}
-
-export const parseLatestLiveItemInfo = (parsedLiveItems: ParsedLiveItem[]) => {
-  let liveItemLatestPubDate = null
-  let liveItemLatestTitle = ''
-  let liveItemLatestImageUrl = ''
-  for (const parsedLiveItem of parsedLiveItems) {
-    const liveItemStatus = parsedLiveItem.status?.toLowerCase()
-    if (
-      liveItemStatus === 'live'
-      && (
-        !liveItemLatestPubDate
-        || new Date(parsedLiveItem.start as any) > new Date(liveItemLatestPubDate)
-      )
-    ) {
-      liveItemLatestPubDate = parsedLiveItem.start
-      liveItemLatestTitle = parsedLiveItem.episode.title || 'Untitled Livestream'
-      liveItemLatestImageUrl = parsedLiveItem.episode.imageURL || ''
-    }
-  }
-  return { liveItemLatestImageUrl, liveItemLatestPubDate, liveItemLatestTitle }
+  // column dates
+  createdAt: Date
+  updatedAt: Date
 }
