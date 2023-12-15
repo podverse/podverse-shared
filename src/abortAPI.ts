8ddaca81db4b539d9171bf270c7934847c5dae27
Mitch Downey <mitchldowney@gmail.com>
2023-12-15 00:45:54 -0600

    Update abortAPI.ts

--- src/abortAPI.ts
+++ src/abortAPI.ts
@@ -1,3 +1,8 @@
+export type AbortAPI = {
+  abortController: AbortController
+  abortTimeout: NodeJS.Timeout
+}
+
 export const generateAbortAPI = () => {
   const abortController = new AbortController()
   const abortTimeLimit = 60000
